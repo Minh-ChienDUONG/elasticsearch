c0001c5ac8664c5e680f625383b7de3761deb255
Chien DUONG <chienduong90@gmail.com>
2021-03-01 19:57:24 +0100

    elastic search demo

---
+++ src/main/java/com/minhchien/elasticsearch/book/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.minhchien.elasticsearch.book.repository;
+
+import com.minhchien.elasticsearch.book.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface BookRepository extends ElasticsearchRepository<Book, String> {
+
+    Page<Book> findByAuthor(String author, Pageable pageable);
+
+    List<Book> findByTitle(String title);
+
+    Book findOne(String id);
+}
